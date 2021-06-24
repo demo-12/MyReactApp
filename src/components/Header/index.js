767080433f0525ca2264aa692dfab98d0b920d9c
肖兵兵 <415078117@qq.com>
2021-06-24 11:29:45 +0800

    init app

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { PageHeader } from 'antd';
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <PageHeader
+                    className="site-page-header"
+                    onBack={() => null}
+                    title="Title"
+                    subTitle="This is a subtitle"
+                />,
+            </div>
+        )
+    }
+}
