ac2274823e48c4d2e6a87c04937ab6e997d00345
demo-12 <76669605+demo-12@users.noreply.github.com>
2021-06-30 14:58:34 +0800

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -9,6 +9,6 @@ const { override, fixBabelImports, addLessLoader } = require('customize-cra');
    }),
    addLessLoader({
      javascriptEnabled: true,
-     modifyVars: { '@primary-color': '#1DA57A' },
+     modifyVars: { '@primary-color': '#f0d50e' },
    }),
  );
